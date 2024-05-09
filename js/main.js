30aed02e3d0aa6d03c2af5d08992492fa0093c8a
jasonbai <byygkcg@163.com>
2024-05-09 09:28:46 +0800

    结束测试

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 $(window).on('load', function () {
 
-  console.log('https://gitee.com/lubanseven/lubanseven/raw/master/白延云的简历V20.pdf')
+  console.log('https://jasonbai008.github.io/lubanseven/白延云的简历V20.pdf')
   /*------------------
     Preloder
   --------------------*/
